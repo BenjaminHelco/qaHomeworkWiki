c0a8dc56967e5c491ec7f601c606c2357c6a8713
Benjamin Helco <benjaminhelco@gmail.com>
2022-11-18 20:09:54 -0700

    2.6 Homework Updated

--- 2.6/employeeManagerTest.test.ts
+++ 2.6/employeeManagerTest.test.ts
@@ -1,4 +1,4 @@
-import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
+import {Builder, By, Capabilities, until, WebDriver, } from "selenium-webdriver";
   const chromedriver = require("chromedriver");
 
   const driver: WebDriver = new Builder()
@@ -9,11 +9,36 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
       driver: WebDriver;
       url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
         //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
+    header: By = By.css('.titleText')
+    addEmployee: By = By.name('addEmployee')
+    newEmployee: By = By.xpath('//li[text()= "New Employee"]')
+    nameInput: By = By.css('[name= "nameEntry"]')
+    phoneInput: By = By.name('phoneEntry')
+    titleInput: By = By.name('titleEntry')
+    saveBtn: By = By.xpath('//button[@id="saveBtn"]')
+    constructor(driver: WebDriver) {
+        this.driver = driver   
+    }
+    async navigate() {
+        await this.driver.get(this.url)
+        await this.driver.wait(until.elementLocated(this.header))
+    }
+    async click(elementBy: By) {
+        await this.driver.wait(until.elementLocated(elementBy))
+        return (await this.driver.findElement(elementBy)).click()
+    }
+    async sendKeys(elementBy: By, keys) {
+        await this.driver.wait(until.elementLocated(elementBy))
+        return this.driver.findElement(elementBy).sendKeys(keys)
+    }
+
   }
+
+  const emPage = new employeePage(driver);
 
   describe("Employee Manger Test", () => {
       beforeEach(async () => {
-          await employeePage.navigate();
+          await emPage.navigate();
       })
       afterAll(async () => {
           await driver.quit()
@@ -24,13 +49,14 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
           await driver.findElement(emPage.newEmployee).click()
           await driver.findElement(emPage.nameInput).click()
           await driver.findElement(emPage.nameInput).clear()
-          await driver.findElement(emPage.nameInput).sendKeys("Change this")
+          await driver.findElement(emPage.nameInput).sendKeys("Heath Ledger")
           await driver.findElement(emPage.phoneInput).click()
           await driver.findElement(emPage.phoneInput).clear()
-          await driver.findElement(emPage.phoneInput).sendKeys("Change this")
+          await driver.findElement(emPage.phoneInput).sendKeys("1231231234")
           await driver.findElement(emPage.titleInput).click()
           await driver.findElement(emPage.titleInput).clear()
-          await driver.findElement(emPage.titleInput).sendKeys("Change this")
+          await driver.findElement(emPage.titleInput).sendKeys("The Real Joker")
   })
+})
 
   /* this is a commment */
